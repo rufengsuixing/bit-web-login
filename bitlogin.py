22bb9f8c6402f0a3755a47ecfb34bb6c8e760da1
rufengsuixing001 <1985151814@qq.com>
2017-11-25 11:39:05 +0800

    add powershell

--- bitlogin.py
+++ bitlogin.py
@@ -1,37 +1,3 @@
-﻿import sys,queue,os
-from Crypto.Cipher import AES
-from binascii import b2a_hex, a2b_hex
-mishi='example1nfi79snf'
-class prpcrypt():
-    def __init__(self, key):
-        self.key = key
-        self.mode = AES.MODE_CBC
-    def encrypt(self, text):
-        cryptor = AES.new(self.key, self.mode, self.key)
-        length = 16
-        count = len(text)
-        add = length - (count % length)
-        text = text + ('\0' * add)
-        self.ciphertext = cryptor.encrypt(text)
-        return b2a_hex(self.ciphertext)
-    def decrypt(self, text):
-        cryptor = AES.new(self.key, self.mode, self.key)
-        plain_text = cryptor.decrypt(a2b_hex(text))
-        return plain_text.rstrip(b'\0')
-def jiami():
-    pc = prpcrypt(mishi)
-    print("-------生成密码程序----------")
-    username = input("请输入用户名：")
-    u = pc.encrypt(username)
-    password = input("请输入密码：")
-    p = pc.encrypt(password)
-    r=os.popen('wmic csproduct get UUID')
-    info=r.readlines()
-    b = pc.encrypt(username+info[2].strip()+password)
-    f = open('naprd','wb')
-    f.write(u+b'\n'+p+b'\n'+b)
-    f.close()
-    print('已生成本机唯一密匙，文件被拿走也没用')
 def link(username,password):
     import requests
     url = 'http://10.0.0.55:802/include/auth_action.php'
@@ -50,25 +16,6 @@ def link(username,password):
     r = requests.post(url, headers=headers,data=payload)
     r.encoding="UTF-8"
     return r.text
-def jiemi():
-    pc = prpcrypt(mishi)
-    try:
-        f = open('naprd','rb')
-        u=f.readline()
-        p=f.readline()
-        b=f.readline()
-        f.close()
-    except:
-        raise
-    username=pc.decrypt(u.strip()).decode()
-    password=pc.decrypt(p.strip()).decode()
-    r=os.popen('wmic csproduct get UUID')
-    info=r.readlines()
-    bn = pc.encrypt(username+info[2].strip()+password)
-    if bn==b.strip():
-        return [username,password]
-    else:
-        raise ValueError("invavid id")
 def down(username,passwd):
     import requests
     url = 'http://10.0.0.55:802/include/auth_action.php'
@@ -87,38 +34,6 @@ def down(username,passwd):
     r = requests.post(url, headers=headers,data=payload)
     r.encoding="UTF-8"
     return r.text
-def main(argv=''):
-    if len(argv)==2:
-        if argv[1]=='jiami':
-            jiami()
-            exit()
-    try:
-        [username,password]=jiemi()
-    except IOError:
-        print('读写错误，是否生成密码文件')
-        jiami()
-        input("按任意键登陆")
-        main()
-        exit()
-    except ValueError:
-        print('新机器请重新生成密码')
-        jiami()
-        input("按任意键登陆")
-        main()
-        exit()
-    except:
-        print('未知错误')
-        exit()
-    if len(argv)==2:
-        if argv[1]=='logout':
-            text=down(username,password)
-            print(text)
-            input("按任意键关闭")
-            exit()
-    text=link(username,password)
-    print(text)
-    input("按任意键关闭")
-    exit()
-        
+
 if __name__ == '__main__':
-    main(sys.argv)
+    link(sys.argv)
---
+++ q.ps1
@@ -0,0 +1,59 @@
+﻿$username=""
+$password=""
+function utf16_utf8 ($x) 
+{ 
+$data = [System.Text.Encoding]::GetEncoding('utf-16').GetBytes($x)
+    $res=@() 
+    foreach ($n in $data)
+    { 
+    if ($n -ne 0)
+        {$res+=$n
+        }
+    }
+$data = [System.Text.Encoding]::GetEncoding('utf-8').GetString($res)
+return $data
+}
+$bodyl = @{
+    "action"="login"
+    "username"=$username
+    "password"=$password
+    "ac_id"="1"
+    "user_ip"=""
+    "nas_ip"=""
+    "user_mac"=""
+    "save_me"="0"
+    "ajax"="1"
+}
+$bodyo = @{
+    "action"="logout"
+    "username"=$username
+    "password"=$password
+    "ac_id"="1"
+    "user_ip"=""
+    "nas_ip"=""
+    "user_mac"=""
+    "save_me"="0"
+    "ajax"="1"
+}
+$ie2 =Invoke-WebRequest http://www.baidu.com -Method GET -MaximumRedirection 0
+while ($ie2.StatusCode -ne 200)
+    {
+    if ($fan.Contains("online"))
+        {
+        $ie =Invoke-WebRequest http://10.0.0.55:802/include/auth_action.php -Method POST -Body $bodyo
+        $fan=utf16_utf8 ($ie.Content) 
+        $fan
+        sleep 1
+        }
+    $ie =Invoke-WebRequest http://10.0.0.55:802/include/auth_action.php -Method POST -Body $bodyl
+    $fan=utf16_utf8 ($ie.Content) 
+    $fan
+    if ($fan.Contains("请填写"))
+    {echo "你没填全"
+        break
+    }
+    sleep 1
+    $ie2 =Invoke-WebRequest http://www.baidu.com -Method GET -MaximumRedirection 0
+    }
+    '程序已停止'
+    $null = [System.Console]::ReadKey()
