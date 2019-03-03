8226b83388ccf5e1f33cffa5a54961673c0315a7
rufengsuixing <1985151814@qq.com>
2019-03-03 11:17:21 +0800

    Update setdebian.sh

--- setdebian.sh
+++ setdebian.sh
@@ -13,7 +13,7 @@ sudo apt-get install libssl1.0
 sudo chmod 777 /var/log
 sudo chmod 777 /var/run
 cd "/mnt/c/Program Files"
-sudo wget https://github.com/pymumu/smartdns/releases/download/Release21/smartdns.1.2019.03.02-1141.x86_64.tar.gz
+sudo wget --no-check-certificate https://github.com/pymumu/smartdns/releases/download/Release21/smartdns.1.2019.03.02-1141.x86_64.tar.gz
 tar -zxvf smartdns.1.2019.03.02-1141.x86_64.tar.gz
 rm smartdns.1.2019.03.02-1141.x86_64.tar.gz
 cd smartdns/etc/smartdns
